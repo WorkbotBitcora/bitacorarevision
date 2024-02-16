06684cc6ccf5d8000c2d97927fdea1a2707c2472
Juanjose0017 <110146287+Juanjose0017@users.noreply.github.com>
2024-02-15 19:18:00 -0500

    Revision

---
+++ uco/src/main/java/co/edu/uco/BitacorarevisionApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitacorarevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitacorarevisionApplication.class, args);
+	}
+
+}
